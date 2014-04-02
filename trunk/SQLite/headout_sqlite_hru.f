18d53c039655465bbabf7b972a06ed589ba19125
hawklorry@gmail.com <hawklorry@gmail.com@601f0089-d405-1b67-f0de-b205494e7d3d>
2014-04-02 15:07:01 +0000

    AAFC Simplify the comments in all headout_sqlite_* subroutines.

--- trunk/SQLite/headout_sqlite_hru.f
+++ trunk/SQLite/headout_sqlite_hru.f
@@ -1,57 +1,9 @@
       subroutine headout_sqlite_hru
 
 !!     ~ ~ ~ PURPOSE ~ ~ ~
-!!     this subroutine writes the headings to the major output files
+!!     create table hru in SQLite database
+!!     The file handle for output.hru is 28
 
-!!    ~ ~ ~ INCOMING VARIABLES ~ ~ ~
-!!    name        |units         |definition
-!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-!!    hedb(:)     |NA            |column titles in subbasin output files
-!!    hedr(:)     |NA            |column titles in reach output files
-!!    hedrsv(:)   |NA            |column titles in reservoir output files
-!!    heds(:)     |NA            |column titles in HRU output files
-!!    hedwtr(:)   |NA            |column titles in HRU impoundment output
-!!                               |file
-!!    icolb(:)    |none          |space number for beginning of column in 
-!!                               |subbasin output file
-!!    icolr(:)    |none          |space number for beginning of column in
-!!                               |reach output file
-!!    icolrsv(:)  |none          |space number for beginning of column in
-!!                               |reservoir output file
-!!    icols(:)    |none          |space number for beginning of column in
-!!                               |HRU output file
-!!    ipdvab(:)   |none          |output variable codes for output.sub file
-!!    ipdvar(:)   |none          |output variable codes for .rch file
-!!    ipdvas(:)   |none          |output variable codes for output.hru file
-!!    isproj      |none          |special project code:
-!!                               |1 test rewind (run simulation twice)
-!!    itotb       |none          |number of output variables printed (output.sub)
-!!    itotr       |none          |number of output variables printed (.rch)
-!!    itots       |none          |number of output variables printed (output.hru)
-!!    msubo       |none          |maximum number of variables written to
-!!                               |subbasin output file (output.sub)
-!!    mhruo       |none          |maximum number of variables written to 
-!!                               |HRU output file (output.hru)
-!!    mrcho       |none          |maximum number of variables written to
-!!                               |reach output file (.rch)
-!!    prog        |NA            |program name and version
-!!    title       |NA            |title lines from file.cio
-!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-!!    ~ ~ ~ LOCAL DEFINITIONS ~ ~ ~
-!!    name        |units         |definition
-!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-!!    ilen        |none          |width of data columns in output file
-!!    j           |none          |counter
-!!    ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-
-!!    ~ ~ ~ SUBROUTINES/FUNCTIONS CALLED ~ ~ ~
-!!    header
-
-!!    ~ ~ ~ ~ ~ ~ END SPECIFICATIONS ~ ~ ~ ~ ~ ~
-
-      !!File handle = 28
       use parm
 
       integer :: j
