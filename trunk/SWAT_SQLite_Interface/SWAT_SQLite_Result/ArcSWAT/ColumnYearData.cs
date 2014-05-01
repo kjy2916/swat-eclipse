9cf5cfe537edf78037b50702d0891e0005f76c9a
hawklorry@gmail.com <hawklorry@gmail.com@601f0089-d405-1b67-f0de-b205494e7d3d>
2014-05-01 18:25:31 +0000

    University Add class ColumnYearData as the base class of SWATUnitColumnYearResult and SWATUnitColumnYearObservationData.

---
+++ trunk/SWAT_SQLite_Interface/SWAT_SQLite_Result/ArcSWAT/ColumnYearData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace SWAT_SQLite_Result.ArcSWAT
+{
+    public abstract class ColumnYearData
+    {
+        public ColumnYearData(string col, int year)
+        {            
+            _col = col;
+            _year = year;
+            _id = getUniqueResultID(col, year);
+        }
+
+        protected string _col = null;
+        protected int _year = -1;
+        protected string _id = null;
+        protected DataTable _table = null;
+        protected Statistics _stat = null;
+
+        public string Column { get { return _col; } }
+        public string ID { get { return _id; } }
+        public int Year { get { return _year; } }
+        public Statistics Statistics { get { if (_stat == null) _stat = new Statistics(Table, _col); return _stat; } }
+        public DataTable Table { get { read(); return _table; } }
+
+        protected abstract void read();        
+
+        public static string getUniqueResultID(string col, int year)
+        {
+            string combineCol = col.Trim();
+            if (year > 0) combineCol += "_" + year.ToString();
+            return combineCol;
+        }
+    }
+}
