036e1896e223d7d60e021da206e9e36a5664474c
hawklorry@gmail.com <hawklorry@gmail.com@601f0089-d405-1b67-f0de-b205494e7d3d>
2014-05-02 20:41:01 +0000

    University 1. Add PerformanceView to show performance of model in one window, i.e. r2 and nse of selected swat units compared with observed data. 2. Add ScenarioComparasionReportView to report the difference between two scenarios.

---
+++ SWAT_SQLite_Interface/SWAT_SQLite_Result/PerformanceView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SWAT_SQLite_Result
+{
+    /// <summary>
+    /// Show performance of all SWAT units (with observed data) compared to observed data in one interface
+    /// </summary>
+    public partial class PerformanceView : UserControl
+    {
+        public PerformanceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SWAT_SQLite_Interface/SWAT_SQLite_Result/ScenarioComparasionReportView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SWAT_SQLite_Result
+{
+    /// <summary>
+    /// List R2 and NSE for all HRUs, Subbasins and Reaches in table
+    /// May take a little bit longer for big model.
+    /// </summary>
+    public partial class ScenarioComparasionReportView : UserControl
+    {
+        public ScenarioComparasionReportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
