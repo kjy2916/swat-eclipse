71c990c708235961df6986c6dbc06425dd3fd1d3
hawklorry@gmail.com <hawklorry@gmail.com@601f0089-d405-1b67-f0de-b205494e7d3d>
2014-05-22 18:46:40 +0000

    Add SWAT 2009 Rev 445 to compare results from SWAT 2009 and SWAT 2012.

--- SWAT_SQLite_Interface/SWAT_SQLite_Result/ArcSWAT/SWATModelType.cs
+++ SWAT_SQLite_Interface/SWAT_SQLite_Result/ArcSWAT/SWATModelType.cs
@@ -8,8 +8,9 @@ namespace SWAT_SQLite_Result.ArcSWAT
     public enum SWATModelType
     {
         SWAT_488 = 0,
-        SWAT_622 = 1,
-        CanSWAT = 2,
+        SWAT_445 = 1,
+        SWAT_622 = 2,
+        CanSWAT = 3,
         UNKNOWN = -1
     }
 }
